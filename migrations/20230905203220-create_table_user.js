d67c9585444a43fa648342d14db4fb19164620f9
David-Rosas <davidrosas1987@gmail.com>
2023-09-05 15:45:27 -0500

    added migrations user and roles

---
+++ migrations/20230905203220-create_table_user.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      codeRecover: {
+        type: Sequelize.STRING,
+      },
+      rol_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'roles',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  }
+};
